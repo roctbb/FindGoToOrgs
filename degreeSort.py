f4afb1debd93cadcf8f3a10abda215f9fc4c54c3
roctbb <roctbb@gmail.com>
2018-07-24 18:14:51 +0300

    удалил правильный файл

--- degreeSort.py
+++
@@ -1,7 +0,0 @@
-import networkx as nx
-
-G = nx.read_gexf('friends.gexf')
-i = 0
-for member in sorted(G.nodes(), key=lambda x:G.degree(x), reverse=True):
-    print(i,':', member, '-', G.degree(member))
-    i += 1
